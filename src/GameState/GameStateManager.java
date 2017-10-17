4a4f2c5af0c88fd6ef0b68d1b9cf38972a700da6
Carterahart18 <cah12858@uga.edu>
2017-10-17 10:55:18 -0400

    Add runnable and drawable structure to the game loop
    * Implement a thread for the run method to run on
    * Implement graphics for the draw method to draw to
    * Draw graphics to JPanel at the end of each game loop
    * Comment out game state impl (currently give NPE)

--- src/GameState/GameStateManager.java
+++ src/GameState/GameStateManager.java
@@ -1,6 +1,6 @@
 package GameState;
 
-import java.awt.*;
+import java.awt.Graphics2D;
 import java.util.ArrayList;
 
 public class GameStateManager {
@@ -16,6 +16,24 @@ public class GameStateManager {
         currentState = 0;
     }
 
-    public void setState(int state) {
+    public void setState(int newState) {
+        currentState = newState;
+        //gameStates.get(currentState).init();
+    }
+
+    public void update() {
+        //gameStates.get(currentState).update();
+    }
+
+    public void draw(Graphics2D g) {
+        //gameStates.get(currentState).draw(g);
+    }
+
+    public void keyPressed(int k){
+        //gameStates.get(currentState).keyPressed(k);
+    }
+
+    public void keyReleased(int k){
+        //gameStates.get(currentState).keyReleased(k);
     }
 }
--- src/Main/Game.java
+++ src/Main/Game.java
@@ -2,23 +2,25 @@ package Main;
 
 import GameState.GameStateManager;
 
-import javax.swing.*;
+import javax.swing.JPanel;
 import java.awt.*;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.awt.image.BufferedImage;
 
-@SuppressWarnings("serial")
 public class Game extends JPanel implements Runnable, KeyListener {
 
     public static final int WIDTH = 800;
     public static final int HEIGHT = 600;
 
     private boolean running;
-
-    private int FPS = 30;
+    private int FPS = 60;
     private long drawIntervalInMilli = 1000 / FPS;
 
+    private Thread thread;
     private GameStateManager gameStateManager;
+    private BufferedImage screen;
+    private Graphics2D graphics;
 
     public Game() {
         super();
@@ -27,47 +29,94 @@ public class Game extends JPanel implements Runnable, KeyListener {
         requestFocus();
     }
 
+    /**
+     * Called automatically by the toolkit.
+     * Makes this Component displayable by connecting it to a native screen resource.
+     */
+    public void addNotify() {
+        super.addNotify();
+
+        // Here we want to set up out interfaces
+        if(thread == null) {
+
+            // This sets up the key listener
+            addKeyListener(this);
+
+            // This actually starts the run method on the thread.
+            thread = new Thread(this);
+            thread.start();
+        }
+    }
+
+    /**
+     * This method holds the primary game loop.
+     */
     public void run() {
         init();
 
+        long begin;
 
         // **** MAIN GAME LOOP **** //
-
         while (running) {
 
+            begin = System.currentTimeMillis();
+
             update();
-            draw();
+            draw(graphics);
+            drawJPanel();
 
-            // We will need some math to sleep for a give amount of time to maintain 30 FPS
+            sleep(begin);
+
         }
-
     }
 
-    public void init() {
+    /**
+     * Calculates how long the game loop took to execute (long elapsed) and then finds how much time is left to
+     * to maintain a constant interval between loops (long difference). The function then sleeps for that many
+     * milliseconds.
+     *
+     * @param begin The time the current game loop iteration started, in milliseconds
+     */
+    private void sleep(long begin){
+        long elapsed = System.currentTimeMillis() - begin;
+        long difference = drawIntervalInMilli - elapsed;
+        if(difference > 0) {
+            try {
+                Thread.sleep(difference);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private void init() {
         running = true;
         gameStateManager = new GameStateManager();
+        screen = new BufferedImage(WIDTH, HEIGHT, BufferedImage.TYPE_INT_RGB);
+        graphics = (Graphics2D) screen.getGraphics();
     }
-
 
     private void update(){
-
+        gameStateManager.update();
     }
 
-    private void draw() {
-
+    private void draw(Graphics2D graphics) {
+        gameStateManager.draw(graphics);
     }
 
-    public void keyTyped(KeyEvent e) {
-
+    private void drawJPanel() {
+        Graphics jPanelGraphics = getGraphics();
+        jPanelGraphics.drawImage(screen, 0, 0, WIDTH, HEIGHT, null);
+        jPanelGraphics.dispose();
     }
 
+    public void keyTyped(KeyEvent e) {}
 
     public void keyPressed(KeyEvent e) {
-
+        gameStateManager.keyPressed(e.getKeyCode());
     }
 
-
     public void keyReleased(KeyEvent e) {
-
+        gameStateManager.keyReleased(e.getKeyCode());
     }
 }
